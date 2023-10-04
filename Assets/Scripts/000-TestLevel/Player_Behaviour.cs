b5507c41d0726b9d30198297a392f15be43d9095
Volkol23 <adriguiu97@gmail.com>
2023-10-04 20:46:58 +0200

    Movement Player
    Movement of the player using Character Controller

--- Assets/Scripts/000-TestLevel/Player_Behaviour.cs
+++ Assets/Scripts/000-TestLevel/Player_Behaviour.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public class Player_Behaviour : MonoBehaviour
 {
+    [Header("Movement Variables")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float speed;
+    [SerializeField] private float acceleration;
+
+    [Header("Jump variables")]
+    [SerializeField] private float gravity;
+
+    private Vector3 finalVelocity = Vector3.zero;
+    private Vector3 direction = Vector3.zero;
+
+    //Components of the gameObject
     private CharacterController characterController;
+
     private void Awake()
     {
+        //Initialize components
         characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        
+        HandleInputs();
+        HandleMovement();
     }
 
     private void HandleInputs()
     {
 
+        direction = +Input_Manager._INPUT_MANAGER.GetMovement().x * transform.right + Input_Manager._INPUT_MANAGER.GetMovement().y * transform.forward;
+
+        
+        direction.Normalize();
+
+        finalVelocity.x = direction.x * speed;
+        finalVelocity.z = direction.z * speed;
     }
 
     private void HandleMovement()
     {
-
+        characterController.Move(finalVelocity * Time.deltaTime);
     }
 }
